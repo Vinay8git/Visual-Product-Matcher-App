d0c37d62a49398ba97f63b39fe97b0b2e9f63fdd
Vinay Yadav <144814325+Vinay8git@users.noreply.github.com>
2025-08-31 19:15:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,7 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST || 'localhost'
 const PY_BIN = process.env.PYTHON_BIN || 'python';
 
 // Middleware
@@ -157,5 +158,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://${HOST}:${PORT}`);
 });
